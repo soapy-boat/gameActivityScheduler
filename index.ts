73ff1edcb667a56573ecc14741053f8e8dfc304d
Zoe Coughlan <zjc7@njit.edu>
2025-04-17 22:35:55 -0400

    index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "gameActivityScheduler",
+    description: "Turns on and off game activity within user-specified windows based on system time.",
+    authors: [{ name: "soapyboat", id: 219534600135770113n }],
+});
